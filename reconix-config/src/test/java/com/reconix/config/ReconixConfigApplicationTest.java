df323f37ebf7e83d2bc74921a3497b87cd5b607b
RafaelSantana-Dev <rafael.s.chaves7@gmail.com>
2026-03-12 19:21:54 -0300

    feat(config): add SecurityConfig, Dockerfile, Maven Wrapper and context load test

---
+++ reconix-config/src/test/java/com/reconix/config/ReconixConfigApplicationTest.java
@@ -0,0 +1,14 @@
+package com.reconix.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconixConfigApplicationTest {
+
+    @Test
+    @DisplayName("Deve carregar o contexto do Config Server com sucesso")
+    void contextLoads() {
+    }
+}

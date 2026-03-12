b37dbeb7173f63df0a0d32c1bbd38ea01b09cf01
RafaelSantana-Dev <rafael.s.chaves7@gmail.com>
2026-03-12 19:33:46 -0300

    feat(gateway): add Dockerfile, Maven Wrapper and context load test

---
+++ reconix-gateway/src/test/java/com/reconix/gateway/ReconixGatewayApplicationTest.java
@@ -0,0 +1,14 @@
+package com.reconix.gateway;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconixGatewayApplicationTest {
+
+    @Test
+    @DisplayName("Deve carregar o contexto do API Gateway com sucesso")
+    void contextLoads() {
+    }
+}

65bcae549adf898f543176966fcec9bb20212291
RafaelSantana-Dev <rafael.s.chaves7@gmail.com>
2026-03-12 19:28:59 -0300

    feat(gateway): add main application class with @EnableDiscoveryClient

---
+++ reconix-gateway/src/main/java/com/reconix/gateway/ReconixGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.reconix.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ReconixGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReconixGatewayApplication.class, args);
+    }
+}
